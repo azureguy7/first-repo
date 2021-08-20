29ac52d3c75d78c4f4eaed2529069dabf8309fe4
azureguy7 <azureguy7@gmail.com>
2021-08-20 17:11:06 +0900

    feat: Change inner var. and string from 'Hello' to 'Data'
    inner var.: i to num

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
-for i in range(1,1+10):
-    if i%2==0:
-        print('Hello')
+for num in range(1,1+10):
+    if num%2==0:
+        print('Data')
+
+
+
+#for i in range(1,1+10):
+#    if i%2==0:
+#        print('Hello')
 
